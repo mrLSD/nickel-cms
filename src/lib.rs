80119fc75d2ed896fc9e5aa1e4341bcfc6b168c8
Evgeny Ukhanov <mrlsd@ya.ru>
2017-01-02 16:42:33 +0200

    Mount point for routers

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,11 @@
 
 use nickel::{Nickel, Mountable, StaticFilesHandler};
 
+mod admin;
+
 /// Build all routers rule
 pub fn routers(server: &mut Nickel<()>) {
+    server.mount("/admin/", admin::routers());
+
     server.mount("/", StaticFilesHandler::new("assets/"));
 }
