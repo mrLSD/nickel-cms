d35127eda7ba17aa6169fa2176a961bb95f28f80
Evgeny Ukhanov <mrlsd@ya.ru>
2017-02-20 09:12:33 +0200

    Admin pages create

--- src/admin/handlers/pages.rs
+++ src/admin/handlers/pages.rs
@@ -6,3 +6,10 @@ pub fn get_main<'mw>(_: &mut Request<Config>, res: Response<'mw, Config>) -> Mid
     header.action = "pages";
     render(res, |o| templates::admin::pages::index(o, &header))
 }
+
+pub fn get_create<'mw>(_: &mut Request<Config>, res: Response<'mw, Config>) -> MiddlewareResult<'mw, Config> {
+    let mut header = HeaderData::new();
+    header.title = "Create page";
+    header.action = "pages";
+    render(res, |o| templates::admin::pages::create(o, &header))
+}
