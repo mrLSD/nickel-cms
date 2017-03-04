d4bc9b69a3dd26ac54e7e2a51c8d604d48c76726
Evgeny Ukhanov <mrlsd@ya.ru>
2017-03-05 01:56:27 +0200

    Testing Header sturct for Templates

--- src/admin/handlers/main.rs
+++ src/admin/handlers/main.rs
@@ -11,8 +11,14 @@ pub use nickel::{
 use middleware::render;
 use templates;
 use config::{Config};
-use super::*;
+
+pub struct HeaderData<'a> {
+    pub title: &'a str,
+}
 
 pub fn get_main<'mw>(_req: &mut Request<Config>, res: Response<'mw, Config>) -> MiddlewareResult<'mw, Config> {
-    render(res, |o| templates::admin::main::index(o))
+    let header = HeaderData {
+        title: "Test",
+    };
+    render(res, |o| templates::admin::main::index(o, &header))
 }
--- src/admin/mod.rs
+++ src/admin/mod.rs
@@ -9,17 +9,9 @@ pub use nickel::{
     Response
 };
 pub use config::{Config};
-use middleware::render;
-use templates;
 use tera::{Context};
 use middleware::TEMPLATES;
-mod handlers;
-
-fn handler<'mw>(_req: &mut Request<Config>, res: Response<'mw, Config>) -> MiddlewareResult<'mw, Config> {
-    render(res, |o| templates::hello(o))
-//    let config = req.server_data();
-//    res.send(format!("Server port: {}", config.server.port))
-}
+pub mod handlers;
 
 pub fn routers() -> Router<Config> {
     let mut router = Router::new();
@@ -30,6 +22,5 @@ pub fn routers() -> Router<Config> {
     router.get("/tera", middleware! {
         TEMPLATES.render("hello.html", Context::new()).unwrap()
     });
-    router.get("/config", handler);
     router
 }
