a5888280f496d0311bc3e57f49541cd73e14957d
Evgeny Ukhanov <mrlsd@ya.ru>
2017-03-05 16:50:15 +0200

    backend: added headers data

--- src/admin/handlers/main.rs
+++ src/admin/handlers/main.rs
@@ -14,11 +14,28 @@ use config::{Config};
 
 pub struct HeaderData<'a> {
     pub title: &'a str,
+    pub keyword: &'a str,
+    pub description: &'a str,
+    pub action: &'a str,
 }
 
-pub fn get_main<'mw>(_req: &mut Request<Config>, res: Response<'mw, Config>) -> MiddlewareResult<'mw, Config> {
-    let header = HeaderData {
-        title: "Test",
-    };
+impl<'a> HeaderData<'a> {
+    pub fn new() -> HeaderData<'a> {
+        HeaderData {
+            title: "",
+            keyword: "",
+            description: "",
+            action: "",
+        }
+    }
+    pub fn is_active(&self, action: &str) -> bool {
+        if self.action == action { true } else { false }
+    }
+}
+
+pub fn get_main<'mw>(_: &mut Request<Config>, res: Response<'mw, Config>) -> MiddlewareResult<'mw, Config> {
+    let mut header = HeaderData::new();
+    header.title = "main";
+    header.action = "main";
     render(res, |o| templates::admin::main::index(o, &header))
 }
